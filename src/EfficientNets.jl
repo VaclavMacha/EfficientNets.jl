947ac6d85f7ce6ed80630ffcf8c121e5652e26a1
Vaclav Macha <vaclav.macha92@gmail.com>
2022-05-20 12:05:55 +0200

    Added MBConvBlock

--- src/EfficientNets.jl
+++ src/EfficientNets.jl
@@ -6,6 +6,29 @@ using Flux: @functor
 using Flux: flatten
 using Flux: SkipConnection
 
+# Custom types
+struct ModelParams{T<:Real,I<:Integer}
+    image_size::NTuple{2,I}
+    width_coef::T
+    depth_coef::T
+    n_classes::I
+    depth_divisor::I
+    depth_min::I
+    dropout::T
+    include_top::Bool
+end
+
+struct BlockParams{T<:Real,I<:Integer}
+    repeats::I
+    filters::NTuple{2,I}
+    channels::Pair{I,I}
+    stride::I
+    expand_ratio::I
+    squeeze_ratio::T
+    skip_connection::Bool
+    momentum::T
+    ϵ::T
+end
 
 include("models.jl")
 include("mbconv.jl")
--- src/mbconv.jl
+++ src/mbconv.jl
@@ -1,5 +1,5 @@
-struct MBConv
-    layers
+struct MBConv{T}
+    layers::T
 end
 
 @functor MBConv
@@ -28,7 +28,7 @@ Mobile Inverted Residual Bottleneck Block.
 - `expand_ratio = 1`: Expansion ratio defines the number of output channels `out_channels = input_channels * expansion_ratio`.
 - `squeeze_ratio = 0`: Squeeze-Excitation ratio. Should be in `(0, 1]` range.
 - `skip_connection = false`: Whether to use skip connection and drop connect.
-- `drop_rate = 0`: Dropout rate. Should be in `(0, 1)` range.
+- `dropout = 0`: Dropout rate. Should be in `(0, 1)` range.
 
 # References:
 1. https://arxiv.org/abs/1704.04861 (MobileNet v1)
@@ -39,60 +39,106 @@ function MBConv(
     filter::NTuple{N,Integer},
     channels::Pair{<:Integer,<:Integer};
     stride=1,
-    pad=SamePad(),
-    bias=false,
-    ϵ=1.0f-5,
-    momentum=0.1f0,
     expand_ratio=1,
     squeeze_ratio=0,
     skip_connection=false,
-    drop_rate=0
+    dropout=0,
+    momentum=0.1f0,
+    ϵ=1.0f-5
 ) where {N}
 
-    channels_in, channels_out = channels
-    channels_mid = ceil(Int, channels_in * expand_ratio)
-    channels_squeezed = max(1, ceil(Int, channels_in * squeeze_ratio))
+    ch_in, ch_out = channels
+    ch_mid = ceil(Int, ch_in * expand_ratio)
+    ch_sqz = max(1, ceil(Int, ch_in * squeeze_ratio))
+
+    bias = false
+    pad = SamePad()
 
     # layers
     layers = Chain(
         # Expansion phase (Inverted Bottleneck)
         expand_ratio == 1 ? identity : Chain(
-            Conv((1, 1), channels_in => channels_mid; bias, pad),
-            BatchNorm(channels_mid, swish; ϵ, momentum)
+            Conv((1, 1), ch_in => ch_mid; bias, pad),
+            BatchNorm(ch_mid, swish; ϵ, momentum)
         ),
 
         # Depthwise convolution phase
         Chain(
-            Conv(filter, channels_mid => channels_mid; groups=channels_mid, bias, stride, pad),
-            BatchNorm(channels_mid, swish; ϵ, momentum),
+            Conv(filter, ch_mid => ch_mid; groups=ch_mid, stride, bias, pad),
+            BatchNorm(ch_mid, swish; ϵ, momentum),
         ),
 
         # Squeeze and Excitation layer
         !(0 < squeeze_ratio <= 1) ? identity : SkipConnection(
             Chain(
                 AdaptiveMeanPool((1, 1)),
-                Conv((1, 1), channels_mid => channels_squeezed, swish; pad),
-                Conv((1, 1), channels_squeezed => channels_mid; pad),
+                Conv((1, 1), ch_mid => ch_sqz, swish; pad),
+                Conv((1, 1), ch_sqz => ch_mid; pad),
             ),
             (mx, x) -> sigmoid.(mx) .* x
         ),
 
         # Pointwise convolution phase
         Chain(
-            Conv((1, 1), channels_mid => channels_out; pad, bias),
-            BatchNorm(channels_out; ϵ, momentum),
+            Conv((1, 1), ch_mid => ch_out; bias, pad),
+            BatchNorm(ch_out; ϵ, momentum),
         ),
     )
 
     # Skip connection and drop connect
-    if skip_connection && stride == 1 && channels_in == channels_out
-        if 0 < drop_rate < 1
+    if skip_connection && stride == 1 && ch_in == ch_out
+        if 0 < dropout < 1
             layers = Chain(
                 layers.layers...,
-                Dropout(drop_rate),
+                Dropout(dropout),
             )
         end
         layers = SkipConnection(layers, +)
     end
     return MBConv(layers)
+end
+
+function compute_channels(channels_orig::Integer, params::ModelParams)
+    multiplier = params.width_coef
+    depth_divisor = params.depth_divisor
+
+    if multiplier ≈ 1
+        return channels_orig
+    end
+
+    channels = multiplier * channels_orig
+    channels_new = max(
+        params.depth_min,
+        (floor(Int, channels + depth_divisor / 2) ÷ depth_divisor) * depth_divisor
+    )
+    if channels_new < 0.9 * channels
+        return channels_new + depth_divisor
+    else
+        return channels_new
+    end
+end
+
+function MBConvBlock(bparams::BlockParams, params::ModelParams)
+    channels_in = compute_channels(bparams.channels[1], params)
+    channels_out = compute_channels(bparams.channels[2], params)
+    repeats = ceil(Int, bparams.repeats * params.depth_coef)
+
+    blocks = map(1:repeats) do i
+        ch_in = i == 1 ? channels_in : channels_out
+        ch_out = channels_out
+        stride = i == 1 ? bparams.stride : 1
+
+        return MBConv(
+            bparams.filters,
+            ch_in => ch_out;
+            stride=stride,
+            expand_ratio=bparams.expand_ratio,
+            squeeze_ratio=bparams.squeeze_ratio,
+            skip_connection=bparams.skip_connection,
+            dropout=params.dropout,
+            momentum=bparams.momentum,
+            ϵ=bparams.ϵ
+        )
+    end
+    return Chain(blocks...)
 end
